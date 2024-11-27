80cd5bc0c74bf7b32913df2bed9473cec9de317c
LeoBaudry <leo.baudry@edu.univ-fcomte.fr>
2024-11-27 08:56:25 +0100

    referme les détails suite à un clic sur un summary

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 // *** ACCORDEON ***
+
+const accordeon = document.querySelector(".accordeon");
+
+accordeon.querySelectorAll("details").forEach((details) => {
+    details.querySelector("summary").addEventListener("click", () => {
+        accordeon.querySelectorAll("details").forEach((otherDetails) => {
+            if (otherDetails !== details) {
+                otherDetails.open = false;
+            }
+        });
+    });
+});
